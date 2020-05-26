8035330142e63ba5c960e4eec2f548d7ba402cd5
Michael Melikov <coutaq@gmail.com>
2020-05-26 05:33:27 +0300

    Add project files.

---
+++ Coutaq/EasyTimer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Coutaq
+{
+    public class EasyTimer
+    {
+        TimeSpan ts;
+        Stopwatch stopwatch;
+        public EasyTimer()
+        {
+            stopwatch = new Stopwatch();
+        }
+        public void Start()
+        {
+            stopwatch.Restart();
+        }
+        public void End(String infoText)
+        {
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            Console.WriteLine(infoText + " " + ts.Seconds + ":" + ts.Milliseconds);
+        }
+        public void End()
+        {
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            Console.WriteLine(ts.Seconds + ":" + ts.Milliseconds);
+        }
+        public String Time()
+        {
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            return ts.Seconds + ":" + ts.Milliseconds;
+        }
+        public String Time(String infoText)
+        {
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            return infoText + " " + ts.Seconds + ":" + ts.Milliseconds;
+        }
+    }
+}
